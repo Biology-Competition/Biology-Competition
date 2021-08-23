0588056913dd7b6c4a3f41108218026a48c2efa7
dorazhou1 <64435558+dorazhou1@users.noreply.github.com>
2021-08-23 17:57:23 -0400

    Content changes
    - changed competition info
    - updated the email subscriber form to have confirmation message
    - fixed footer navbar

--- assets/js/formsubmit.js
+++ assets/js/formsubmit.js
@@ -4,6 +4,12 @@ const form = document.forms['submit-to-google-sheet']
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+      .then(response => success(response))
       .catch(error => console.error('Error!', error.message))
 })
+
+function success(response) {
+    console.log('Success!', response);
+    document.getElementById("thanks").style.display = "block";
+    document.getElementById("subscribe-btn").style.display = "none";
+}
