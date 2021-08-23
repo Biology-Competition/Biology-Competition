31d335dc7c1e7a5d5efa6b12dfb80cef46ec0387
dorazhou1 <64435558+dorazhou1@users.noreply.github.com>
2021-08-22 22:46:30 -0400

    Email submit + Minor edits 2.0
    - added the form submit functionality
    - deleted some comments
    - renamed files
    - fixed padding to make it look nicer

---
+++ assets/js/formsubmit.js
@@ -0,0 +1,9 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwAKq2s8A8tgTgCMb680Zuvj3i1QpB3Mr-im5JUIVtZ6Vxfn_O_Qd91gmsLfY503NV8/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+})
